8fc974e5492b70690920b950ce5b1842e7b70b70
Accacio <raccacio@poli.ufrj.br>
2022-09-11 21:40:38 +0200

    add pdf figures

--- code/vulnerabilities.py
+++ code/vulnerabilities.py
@@ -68,11 +68,12 @@ axs.plot(np.arange(0,lastp[k]),np.transpose(theta[1,0:lastp[k],k,1]),color_map[3
 axs.plot(np.arange(0,lastp[k]),np.transpose(theta[0,0:lastp[k],k,2]),color_map[4])
 axs.plot(np.arange(0,lastp[k]),np.transpose(theta[1,0:lastp[k],k,2]),color_map[5])
 
-plt.xlabel('Negotiation step (p)',usetex=True,fontsize=16)
+plt.xlabel('Negotiation step ($p$)',usetex=True,fontsize=16)
 plt.xticks(fontsize = 20)
 plt.yticks(fontsize = 20)
 plt.legend((  '$\\theta_{1_1}$', '$\\theta_{1_2}$', '$\\theta_{2_1}$', '$\\theta_{2_2}$','$\\theta_{3_1}$', '$\\theta_{3_2}$'),ncol=3,fontsize=16)
 plt.savefig("../img/example_theta" +  ".pdf",bbox_inches='tight',facecolor=fig.get_facecolor())
+plt.savefig("../img/example_theta" +  ".png",bbox_inches='tight',facecolor=fig.get_facecolor())
 
 fig, axs = plt.subplots(1, 1,figsize=(7, 3),facecolor=(.0, .0, .0, .0))
 axs.plot(np.arange(0,lastp[k]),np.transpose(lambdaHist[0,0:lastp[k],k,0]),color_map[0])
@@ -84,11 +85,12 @@ axs.plot(np.arange(0,lastp[k]),np.transpose(lambdaHist[1,0:lastp[k],k,2]),color_
 
 axs.plot(np.arange(0,lastp[k]),np.transpose(np.mean(lambdaHist[:,0:lastp[k],k,:],axis=2)),'--k')
 
-plt.xlabel('Negotiation step (p)',usetex=True,fontsize=16)
+plt.xlabel('Negotiation step ($p$)',usetex=True,fontsize=16)
 plt.xticks(fontsize = 20)
 plt.yticks(fontsize = 20)
 plt.legend((  '$\\lambda_{1_1}$', '$\\lambda_{1_2}$', '$\\lambda_{2_1}$', '$\\lambda_{2_2}$','$\\lambda_{3_1}$', '$\\lambda_{3_2}$'),ncol=3,fontsize=16)
 plt.savefig("../img/example_lambda" +  ".pdf",bbox_inches='tight',facecolor=fig.get_facecolor())
+plt.savefig("../img/example_lambda" +  ".png",bbox_inches='tight',facecolor=fig.get_facecolor())
 
 fig, axs = plt.subplots(1, 1,figsize=(7, 3),facecolor=(.0, .0, .0, .0))
 axs.plot(np.arange(0,simK),np.repeat(Wt[0],simK),lineStyle='--',color=color_map[0])
@@ -105,6 +107,7 @@ plt.xticks(fontsize = 20)
 plt.yticks(fontsize = 20)
 plt.legend(('$w_1[k]$', '$x_1[k]$','$w_2[k]$', '$x_2[k]$','$w_3[k]$', '$x_3[k]$'),loc='right',fontsize=16)
 plt.savefig("../img/example_state" +  ".pdf",bbox_inches='tight',facecolor=fig.get_facecolor())
+plt.savefig("../img/example_state" +  ".png",bbox_inches='tight',facecolor=fig.get_facecolor())
 
 
 # plt.show()
